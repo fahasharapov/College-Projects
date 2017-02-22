6ec64614a1ddac43b74c6f5c416bc753d4c614e6
Faha Sharapov <fahasharapov@gmail.com>
2017-02-22 15:37:04 -0500

    Change txt type to cpp

--- 1-Multiples of 3 and 5.cpp
+++ 1-Multiples of 3 and 5.cpp
@@ -5,7 +5,6 @@ using namespace std;
 /********************************************************************
 If we list all the natural numbers below 10 that are multiples of
 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 Find the sum of all the multiples of 3 or 5 below 1000.
 ********************************************************************/
 
@@ -29,5 +28,7 @@ int main()
     }
 
 cout << totalResult << endl;
+
+return 0;
 
 }
